93bd2bc20344ea472d3181c249c05b72e9d63b01
NIkita <mykytabaranov@gmail.com>
2025-09-03 12:30:06 +0300

    refactoring

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,37 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-08-01',
   devtools: { enabled: true },
 
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '96x96',
+          href: '/favicon-96x96.png'
+        },
+        {
+          rel: 'icon',
+          type: 'image/svg+xml',
+          href: '/favicon.svg'
+        },
+        {
+          rel: 'shortcut icon',
+          href: '/favicon.ico'
+        },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/apple-touch-icon.png'
+        },
+        {
+          rel: 'manifest',
+          href: 'site.webmanifest'
+        }
+      ]
+    }
+  },
+
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
