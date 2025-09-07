2d3cb0f16d8d77f4a77044816e0d74e024313070
niqitos <mykytabaranov@gmail.com>
2025-09-07 16:28:10 +0000

    Deploying to gh-pages from @ niqitos/wiring.blog@914dc90fcc5276c03663cdc2ad58e0ef91e825f8 🚀

---
+++ _nuxt/wdqu2YkN.js
@@ -0,0 +1,1 @@
+import{u as o}from"#entry";const u=()=>{const{$i18n:e}=o();return{formatDate:t=>new Date(t).toLocaleDateString(e.locales.value.find(a=>a.code===e.locale.value)?.language,{year:"numeric",month:"short",day:"numeric"})}};export{u};
